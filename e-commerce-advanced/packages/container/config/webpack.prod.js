1aff5f8432c4941ce8306e8c9122ed3817b6dbc5
Dominik Stefancik <dstefancik@gmail.com>
2021-02-08 22:48:00 +0100

    Create a container deploy script

---
+++ e-commerce-advanced/packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const productionDomain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${productionDomain}/marketing/remoteEntry.js`,
+      },
+      // all dependencies which are mentioned in the 'package.json' file will be shared
+      // i.e. webpack makes sure that these dependencies are loaded exactly one time in the browser
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ e-commerce-advanced/packages/marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      // all dependencies which are mentioned in the 'package.json' file will be shared
+      // i.e. webpack makes sure that these dependencies are loaded exactly one time in the browser
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
